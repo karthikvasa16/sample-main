6ea2e8687161fad27b1a1d710d550e3cb7a55591
Durgaprasad <128016655+durgaprasad104@users.noreply.github.com>
2026-01-02 12:30:47 +0530

    Delete server/test_require.js

--- server/test_require.js
+++
@@ -1,7 +0,0 @@
-console.log('Attempting to require ./models...');
-try {
-    const models = require('./models');
-    console.log('Successfully required models:', Object.keys(models));
-} catch (error) {
-    console.error('Failed to require models:', error);
-}
